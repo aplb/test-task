b03cb75606ceaa7dc58f040a8324d68b9ca05483
Alex B <al7bashkatov@gmail.com>
2018-08-05 18:57:34 +0300

    app class to fn component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import AppBar from './components/AppBar';
 import Transactions from './components/Transactions/ListContainer';
 
-// may be useful
-// https://github.com/mui-org/material-ui/blob/dd45ebaa3fa9277c66d83d978da2e113261db458/docs/src/app/components/CodeExample/CodeBlock.js
-
-class App extends Component {
-  componentDidMount() {
-    // fetch('/transaction')
-    //   .then(r => r.json())
-    //   .then(r => console.log('----------', r));
-  }
-  render() {
-    return (
-      <MuiThemeProvider theme={createMuiTheme({})}>
-        <AppBar />
-        <Transactions />
-      </MuiThemeProvider>
-    );
-  }
-}
+const App = () => (
+  <MuiThemeProvider theme={createMuiTheme({})}>
+    <AppBar />
+    <Transactions />
+  </MuiThemeProvider>
+);
 
 export default App;
